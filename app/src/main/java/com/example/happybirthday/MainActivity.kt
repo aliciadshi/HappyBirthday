a067e5002a84da2701adc7da498d637124a5b28b
Alicia Shi <destinyshi@gmail.com>
2023-04-09 15:30:32 -0700

    Changed background and added animated cake

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.happybirthday
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import pl.droidsonroids.gif.GifDrawable
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val imageView: ImageView = findViewById<View>(R.id.birthdayCake) as ImageView
+        val gifDrawable = GifDrawable(resources, R.drawable.happy_birthday_birthday_cake)
+        imageView.setImageDrawable(gifDrawable)
+
     }
 }
